43ad8e22f216e397e07d36c5c2a1f4a2bf1da0cc
g19941sm <75403365+g19941sm@users.noreply.github.com>
2020-12-17 16:28:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,13 @@ function randomValueFromArray(array){
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let storyText2 = '気温は華氏94度だった。  :insertx: は出かけた。  :inserty: に着いた。they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText2 = '気温は華氏40度で外は寒かった。  :insertx: は出かけた。  :inserty: に着いた。:insertz:体重110ポンドの:insertx:は驚いたが、 Bobは驚かなかった。:inserty: ではよくあることだった。';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+let insertX2 = ['スヌーピー','ミッキーマウス','サンタクロース'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
+let insertY2 = ['千駄ヶ谷','ディズニーランド','津田塾大学'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let insertZ2 = ['雪が降っていた。','混雑していた。','雨が降っていた。'];
 
 randomize.addEventListener('click', result);
 
@@ -20,13 +23,18 @@ function result() {
   let newStory2 = storyText2;
 
   let xItem = randomValueFromArray(insertX);
+  let x2Item = randomValueFromArray(insertX2);
   let yItem = randomValueFromArray(insertY);
+  let y2Item = randomValueFromArray(insertY2);
   let zItem = randomValueFromArray(insertZ);
+  let z2Item = randomValueFromArray(insertZ2);
 
   newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':insertx:',xItem);
+  newStory2 = newStory.replace(':insertx:',x2Item);
   newStory = newStory.replace(':inserty:',yItem);
+  newStory2 = newStory.replace(':inserty:',y2Item);
   newStory = newStory.replace(':insertz:',zItem);
+  newStory2 = newStory2.replace(':insertz:',z2Item);
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -34,10 +42,10 @@ function result() {
   }
 
   if(document.getElementById("日本").checked) {
-    const weight = Math.round(300*0.453592) + 'kg';
-    const temperature =  '摂氏'+Math.round((94-32) * 5 / 9) + '度';
-    newStory2 = newStory2.replace('94 fahrenheit',temperature);
-    newStory2 = newStory2.replace('300 pounds',weight);
+    const weight = Math.round(110*0.453592) + 'kg';
+    const temperature =  '摂氏'+Math.round((40-32) * 5 / 9) + '度';
+    newStory2 = newStory2.replace('華氏40度',temperature);
+    newStory2 = newStory2.replace('110ポンド',weight);
   }
 
   story.textContent = newStory;
